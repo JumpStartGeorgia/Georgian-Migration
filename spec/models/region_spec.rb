ad697b522a50971e386ae0a3fb85f2ec532992c1
nwshane <django09@gmail.com>
2016-06-22 15:47:11 +0400

    Small model spec refactor.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe Region do
+  let(:region) { FactoryGirl.build(:region) }
+
   it 'is valid with minimum required attributes' do
-    region = FactoryGirl.build(:region)
     expect(region).to be_valid
   end
 
   describe '#name' do
     it 'is required' do
-      region = FactoryGirl.build(:region)
       region.name = nil
       region.valid?
       expect(region.errors[:name].size).to eq(1)
