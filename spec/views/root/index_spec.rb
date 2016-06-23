d6fb8be4a96051b8f05a71004bd8ac61d1e80e8f
nwshane <django09@gmail.com>
2016-06-23 16:03:08 +0400

    Showing special message on home page if no regions have been created.

---
+++ spec/views/root/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'root/index.html.erb' do
+  context 'when there is a region' do
+    it "displays the region's name" do
+      region = FactoryGirl.create(:region, name: 'Tbilisi')
+      assign(:regions, [region])
+
+      render
+
+      expect(rendered).to have_content 'Tbilisi'
+    end
+  end
+
+  context 'when there is no region' do
+    it 'displays message that there is no region' do
+      assign(:regions, [])
+
+      render
+
+      expect(rendered).to have_content 'No regions have been created.'
+    end
+  end
+end
