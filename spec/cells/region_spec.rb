10c942d5fd8f900d22725f69710c37b0ce106863
nwshane <django09@gmail.com>
2016-06-23 17:38:31 +0400

    Code style fix.

--- spec/cells/region_spec.rb
+++ spec/cells/region_spec.rb
@@ -4,7 +4,7 @@ describe RegionCell do
   it 'renders the name of a region' do
     region = FactoryGirl.create(:region, name: 'Samegrelo')
 
-    html = RegionCell.call(region).call()
+    html = RegionCell.call(region).call
 
     expect(Capybara.string(html)).to have_content 'Samegrelo'
   end
